542009b542c8bec7cadbe464f7b584da43bb8a81
pyrad.asus.msys2 <cailong89@163.com>
2022-08-28 19:09:15 +0800

    Add 2 functions to use universal references
    Examples to show the usage of: decltype(auto) in C++14, and its
    counterpart in C++11

--- src/EffectiveModernCpp/chapter1_item03.cpp
+++ src/EffectiveModernCpp/chapter1_item03.cpp
@@ -9,6 +9,18 @@ namespace chapter_1 {
 namespace item_03 {
 
 /**
+ * @brief A simple function to return a temporary std::vector for testing
+ *        rvalue use
+ *
+ * @return A std::vector, which is an rvalue indeed
+ *
+ */
+std::vector<int> get_temp_ivec() {
+    std::vector<int> int_vec{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    return int_vec;
+}
+
+/**
  * @brief A function to show how to use a function with a trailing return type
  *        to access a value in a container, by using keyword decltype.
  */
@@ -27,6 +39,12 @@ void test_container_access_value() {
     // This line compiles, as decltype(auto) in C++14 returns exactly the same
     // type of ivec[1] (which is actually a reference to its 2nd element)
     authAndAccess_cxx14_rt(ivec, 1) = 200;
+
+    // "get_temp_ivec" returns an rvalue(vector), and authAndAccess_cxx14_rt_uf
+    // returns the 2nd value of that rvalue, then assign it to val
+    auto val0 = authAndAccess_cxx14_rt_uf(get_temp_ivec(), 2);
+    // The following function can be used in C++11
+    auto val1 = authAndAccess_cxx11_rt_uf(get_temp_ivec(), 3);
 }
 
 void test_decltype() {
--- src/EffectiveModernCpp/chapter1_item03.hpp
+++ src/EffectiveModernCpp/chapter1_item03.hpp
@@ -13,7 +13,7 @@ namespace item_03 {
 /**
  * @brief A simple function to show how keyword decltype is used
  *        to declare the return type of a function.
- * 
+ *
  * @param[in] c A container like object
  * @param[in] i The index of the value to be accessed in the container
  *
@@ -22,9 +22,10 @@ namespace item_03 {
  * This trailing return type is in format of C++11.
  *
  * @note, here "wtrt" means "with trailing return type"
+ * @note, "c" can only be bound to an lvalue
  */
 template<typename Container, typename Index>
-auto authAndAccess_wtrt(Container& c, Index i) -> decltype(c[i]) {
+auto authAndAccess_wtrt(Container &c, Index i) -> decltype(c[i]) {
 	// authenticateUser();
 	return c[i];
 }
@@ -32,7 +33,7 @@ auto authAndAccess_wtrt(Container& c, Index i) -> decltype(c[i]) {
 /**
  * @brief A simple function to show how a function's return type is deduced
  *        from keyword auto
- * 
+ *
  * @param[in] c A container like object
  * @param[in] i The index of the value to be accessed in the container
  *
@@ -45,10 +46,11 @@ auto authAndAccess_wtrt(Container& c, Index i) -> decltype(c[i]) {
  *
  * @note, here "wotrt" means "without trailing return type"
  * @note, This format (return type is auto) needs C++14 support.
+ * @note, "c" can only be bound to an lvalue
  *
  */
 template<typename Container, typename Index>
-auto authAndAccess_wotrt(Container& c, Index i) {
+auto authAndAccess_wotrt(Container &c, Index i) {
 	// authenticateUser();
 	return c[i];
 }
@@ -56,7 +58,7 @@ auto authAndAccess_wotrt(Container& c, Index i) {
 /**
  * @brief A simple function to show how a function's return type is deduced
  *        from keyword decltype with auto in its parenthesis.
- * 
+ *
  * @param[in] c A container like object
  * @param[in] i The index of the value to be accessed in the container
  *
@@ -65,12 +67,61 @@ auto authAndAccess_wotrt(Container& c, Index i) {
  *         function authAndAccess_wotrt above.
  *
  * @note, This format (return type is auto) needs C++14 support.
+ * @note, "c" can only be bound to an lvalue
  *
  */
 template<typename Container, typename Index>
-decltype(auto) authAndAccess_cxx14_rt(Container& c, Index i) {
+decltype(auto) authAndAccess_cxx14_rt(Container &c, Index i) {
 	// authenticateUser();
 	return c[i];
+}
+
+/**
+ * @brief A simple function to show how a function's return type is deduced
+ *        from keyword decltype with auto in its parenthesis.
+ *
+ * @param[in] c A container like object
+ * @param[in] i The index of the value to be accessed in the container
+ *
+ * @return The reference to the value being indexed in the container.
+ *         Pay attention to what's the difference between this function and the
+ *         function authAndAccess_wotrt above.
+ *
+ * @note, This format (return type is auto) needs C++14 support.
+ * @note, Now "c" can be bound to eithr an lvalue or an rvalue.
+ * @note, In order to retain the reference-ness of the value accessed, use
+ *        std::forward (perfect forwarding)
+ *
+ */
+template<typename Container, typename Index>
+decltype(auto) authAndAccess_cxx14_rt_uf(Container &&c, Index i) {
+	// authenticateUser();
+	return std::forward<Container>(c)[i]; // Perfect forwarding retains reference-ness
+}
+
+/**
+ * @brief A simple function to show how a function's return type is deduced
+ *        from keyword decltype with auto in its parenthesis.
+ *
+ * @param[in] c A container like object
+ * @param[in] i The index of the value to be accessed in the container
+ *
+ * @return The reference to the value being indexed in the container.
+ *         Pay attention to what's the difference between this function and the
+ *         function authAndAccess_wotrt above.
+ *
+ * @note, This format (return type is auto) can be used in C++11.
+ * @note, Now "c" can be bound to eithr an lvalue or an rvalue.
+ * @note, In order to retain the reference-ness of the value accessed, use
+ *        std::forward (perfect forwarding)
+ *
+ */
+template<typename Container, typename Index>
+auto authAndAccess_cxx11_rt_uf(Container &&c, Index i)
+    -> decltype(std::forward<Container>(c)[i])
+{
+	// authenticateUser();
+	return std::forward<Container>(c)[i]; // Perfect forwarding retains reference-ness
 }
 
 void test_container_access_value();
