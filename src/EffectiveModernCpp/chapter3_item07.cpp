b788559f317dde0af3f4102a2dd7c8ae0afc5b6d
pyrad.asus.msys2 <cailong89@163.com>
2022-09-10 22:00:47 +0800

    Code for item 7
    Finished code samples for item 7

--- src/EffectiveModernCpp/chapter3_item07.cpp
+++ src/EffectiveModernCpp/chapter3_item07.cpp
@@ -10,6 +10,37 @@ namespace item_07 {
 
 int foo::idcnt = 0;
 
+void test_initialize_ways_compared_with_curly_braces() {
+    fprintf(stdout, "----- BEGIN of function %s -----\n", __FUNCTION__);
+    int a(0); // initialize with parenthesis
+    int b = 0; // initialize with a equal sign
+    int c{0}; // initialize with a pair of curly braces
+    int d = {0}; // equals-sign-plus-curly-braces, C++ treat it the same as braces-only version
+
+    foo myfoo(10);
+    foo myfoo2 = myfoo; // initialization with the copy cotr
+
+
+    fprintf(stdout, "----- END of function %s -----\n\n", __FUNCTION__);
+
+
+} // test_initialize_ways_compared_with_curly_braces
+
+void test_class_member_default_values() {
+    utilities::ShowStartEndMsg msgshow(__FUNCTION__);
+
+    // Default values (4 and 5) are overridden by the values in default ctor.
+    ObjDefaultMemberValue myDefaultMemberValue;
+    myDefaultMemberValue.show_me(); // 0 and 1
+
+    // Default values (4 and 5) are used because m_x and m_y are not set in
+    // the copy constructor.
+    ObjDefaultMemberValue myDefaultMemberValue2 = myDefaultMemberValue;
+    myDefaultMemberValue2.show_me(); // 4 and 5
+
+
+} // test_class_member_default_values
+
 void test_ctors() {
     fprintf(stdout, "----- BEGIN of function %s -----\n", __FUNCTION__);
     foo myfoo(10);
@@ -20,12 +51,118 @@ void test_ctors() {
     fprintf(stdout, "----- END of function %s -----\n\n", __FUNCTION__);
 }
 
+void test_equal_sign_cannot_be_used_for_constructing_atomic() {
+    fprintf(stdout, "----- BEGIN of function %s -----\n", __FUNCTION__);
+    std::atomic<int> ai1{0};
+    std::atomic<int> ai2(0);
+    // For std::atomic, Can't use copy constructor, because it is marked as "delete"
+    /* std::atomic<int> ai3 = 0; */
+
+    fprintf(stdout, "----- END of function %s -----\n\n", __FUNCTION__);
+
+} // test_equal_sign_cannot_be_used_for_constructing_atomic
+
+void test_narrowing_conversion() {
+    fprintf(stdout, "----- BEGIN of function %s -----\n", __FUNCTION__);
+    double x = 0, y = 1, z = 2;
+
+    // Can't use curly braces, because narrowing convertion is not allowed
+    // by initialization in curly braces.
+    // Here narrowing convertion happens when trying to convert from
+    // 'double' to 'int'.
+    /* int sum1{x + y + z}; */
+
+    int sum2( x + y + z ); // Ok
+    int sum3 = x + y + z ; // Ok
+    fprintf(stdout, "----- END of function %s -----\n\n", __FUNCTION__);
+} // test_narrawing_conversion
+
+void test_curly_braces_call_default_ctor() {
+    utilities::ShowStartEndMsg MSGSHOW(__FUNCTION__);
+
+    myItemCallDefaultCtor a; // calls default constructor
+    myItemCallDefaultCtor b{}; // calls default constructor
+    myItemCallDefaultCtor c({}); // calls constructor with std::initializer_list
+    myItemCallDefaultCtor d{{}}; // calls constructor with std::initializer_list
+    
+} // test_curly_braces_call_default_ctor
+
+/**
+ * @brief Show how curly braces with a std::initializer_list as ctor's parameter
+ *        results in bad match when contructing objects.
+ *
+ * If there is a ctor which accepts a std::initializer_list, then perhaps it
+ * results in bad match when contructing objects. Even sometimes the narrowing
+ * convertion happens, compiler still tries to match it with the ctor with a
+ * parameter of std::initializer_list, thus leads to compiler error!
+ */
+void test_curly_braces_hijack_with_std_initializer_list() {
+    fprintf(stdout, "----- BEGIN of function %s -----\n", __FUNCTION__);
+    myItem2Ctors a(0, false); // ctor with int & bool
+    myItem2Ctors b(0, 0.1); // ctor with int & double
+    myItem2Ctors c{0, false}; // ctor with int & bool
+    myItem2Ctors d{0, 0.1}; // ctor with int & double
+
+    myItem3Ctors e(0, false); // ctor with int & bool
+    myItem3Ctors f(0, 0.1); // ctor with int & double
+    myItem3Ctors g{0, false}; // Hijack!! ctor with std::initializer_list was called
+    myItem3Ctors h{0, 0.1}; // Hijack!! ctor with std::initializer_list was called
+
+    myItemFloatOp x0(0, false); // Define 2 objects
+    myItemFloatOp x1(0, false); // Define 2 objects
+
+    myItemFloatOp a1(x0); // copy constructor called
+    myItemFloatOp a2{x0}; // Hijack!!! copy constructor not called
+
+    myItemFloatOp a3(std::move(x0)); // copy constructor called
+    myItemFloatOp a4{std::move(x1)}; // Hijack!!! copy constructor not called
+
+    fprintf(stdout, "----- END of function %s -----\n\n", __FUNCTION__);
+} // test_curly_braces_hijack_with_std_initializer_list
+
+
+void test_curly_braces_hijack_with_std_initializer_list_lead_to_compile_errors() {
+    fprintf(stdout, "----- BEGIN of function %s -----\n", __FUNCTION__);
+
+    myItemNarrowConversion w0(0, 5.0); // Ok
+
+    // Compile error, because curly braces with std initializer_list ctor hijack other
+    // ctors, but at the same time, narrowing convertion is not allowed, thus error.
+    /* myItemNarrowConversion w1{0, 5.0}; */
+
+    fprintf(stdout, "----- END of function %s -----\n\n", __FUNCTION__);
+
+} // test_curly_braces_hijack_with_std_initializer_list_lead_to_compile_errors
+
+
+/**
+ * @brief Show usage of curly braces
+ */
+void test_initialize_with_curly_braces() {
+    fprintf(stdout, "----- BEGIN of function %s -----\n", __FUNCTION__);
+
+    test_initialize_ways_compared_with_curly_braces();
+
+    test_class_member_default_values();
+
+    test_equal_sign_cannot_be_used_for_constructing_atomic();
+
+    test_curly_braces_hijack_with_std_initializer_list();
+
+    test_curly_braces_hijack_with_std_initializer_list_lead_to_compile_errors();
+
+    test_curly_braces_call_default_ctor();
+
+    fprintf(stdout, "----- END of function %s -----\n\n", __FUNCTION__);
+} // test_initialize_with_curly_braces
+
 
 
 void test_all() {
     fprintf(stdout, "\n");
     fprintf(stdout, "----- BEGIN of function %s -----\n", __FUNCTION__);
     test_ctors();
+    test_initialize_with_curly_braces();
     fprintf(stdout, "----- END of function %s -----\n\n", __FUNCTION__);
 }
 
--- src/EffectiveModernCpp/chapter3_item07.hpp
+++ src/EffectiveModernCpp/chapter3_item07.hpp
@@ -1,9 +1,12 @@
 #ifndef EFFECTIVE_MODERN_CPP_CHAPTER3_ITEM07
 #define EFFECTIVE_MODERN_CPP_CHAPTER3_ITEM07
 
+#include <initializer_list>
 #include <iostream>
+#include <stdio.h>
 #include <vector>
 #include "../utilities/utilities.hpp"
+#include <atomic>
 
 namespace effective_mordern_cpp {
 
@@ -61,7 +64,177 @@ private:
 }; // end of class foo
 
 
+/**
+ * @brief To show different ways to set the default value for class members
+ */
+class ObjDefaultMemberValue {
+public:
+    ///< If members are not initialized in (default/copy/...) ctors, then
+    ///< the default values when those members variables are defined will
+    ///< be used.
+    ///< Here the 0 and 1 are set to m_x and m_y separately. But if they 
+    ///< are not set in this default ctor, then 4 and 5 will be set to
+    ///< m_x and m_y respectively.
+    ObjDefaultMemberValue() : m_x(0), m_y(1) { }
+    virtual ~ObjDefaultMemberValue () { }
+
+    ///< For this copy constructor, the default values 4 and 5 will be set to
+    ///< m_x and m_y respectively.
+    ObjDefaultMemberValue(const ObjDefaultMemberValue &other) { }
+
+public:
+    int x() const { return m_x; }
+    int y() const { return m_y; }
+
+public:
+    void show_me() const {
+        // fprintf(stdout, "m_x = %d, m_y = %d, m_z = %d\n", m_x, m_y, m_z);
+        fprintf(stdout, "m_x = %d, m_y = %d\n", m_x, m_y);
+    }
+
+private:
+    int m_x{4}; // Use curly braces to initialize member variable's default value
+    int m_y = 5; // Use equal sign to initialize member variable's default value
+    // int m_z(0); // parenthesis can't be used to initialize member variable's default value
+}; // class ObjDefaultMemberValue
+
+
+/**
+ * @brief Difference between myItem2Ctors and myItem3Ctors, is that myItem3Ctors
+ *        has a constructor which accepts std::initializer_list.
+ *        So these 2 constructors are to show how curly braces initialization with
+ *        an std::initializer_list ctor will result in unexpected behaviours.
+ */
+class myItem2Ctors {
+public:
+    myItem2Ctors(int i, bool b) : m_i(i), m_b(b) {
+        fprintf(stdout, "[myItem2Ctors] ctor with Int and Bool\n");
+    }
+    myItem2Ctors(int i, double d) : m_i(i), m_d(d) {
+        fprintf(stdout, "[myItem2Ctors] ctor with Int and Double\n");
+    }
+    virtual ~myItem2Ctors() { }
+
+private:
+    int m_i = 100;
+    bool m_b = true;
+    double m_d = 3.14;
+}; // class myItem2Ctors
+
+/**
+ * @brief See comment in @myItem2Ctors
+ */
+class myItem3Ctors {
+public:
+    myItem3Ctors(int i, bool b) : m_i(i), m_b(b) {
+        fprintf(stdout, "[myItem3Ctors] ctor with Int and Bool\n");
+    }
+    myItem3Ctors(int i, double d) : m_i(i), m_d(d) {
+        fprintf(stdout, "[myItem3Ctors] ctor with Int and Double\n");
+    }
+    myItem3Ctors(std::initializer_list<long double> il) {
+        fprintf(stdout, "[myItem3Ctors] ctor std::initializer_list\n");
+    }
+    virtual ~myItem3Ctors() { }
+
+private:
+    int m_i = 100;
+    bool m_b = true;
+    double m_d = 3.14;
+}; // class myItem3Ctors
+
+class myItemFloatOp {
+public:
+    myItemFloatOp(int i, bool b) : m_i(i), m_b(b) {
+        // fprintf(stdout, "[myItemFloatOp] ctor with Int and Bool\n");
+    }
+    myItemFloatOp(int i, double d) : m_i(i), m_d(d) {
+        // fprintf(stdout, "[myItemFloatOp] ctor with Int and Double\n");
+    }
+    myItemFloatOp(std::initializer_list<long double> il) {
+        fprintf(stdout, "[myItemFloatOp] ctor std::initializer_list\n");
+    }
+    myItemFloatOp(const myItemFloatOp &other) : m_i(other.i()), m_b(other.b()), m_d(other.d()) {
+        fprintf(stdout, "[myItemFloatOp] copy ctor\n");
+    }
+    myItemFloatOp(myItemFloatOp &&other) : m_i(other.i()), m_b(other.b()), m_d(other.d()) {
+        fprintf(stdout, "[myItemFloatOp] move ctor\n");
+    }
+    virtual ~myItemFloatOp() { }
+    operator float() const { return m_d; }
+
+public:
+    int i() const { return m_i; }
+    bool b() const { return m_b; }
+    double d() const { return m_d; }
+
+private:
+    int m_i = 100;
+    bool m_b = true;
+    double m_d = 3.14;
+};
+
+/**
+ * @brief Use to show how ctor with an std::initializer_list hijack other constructors
+ *        (when using curly braces) and might lead to compiler errors
+ */
+class myItemNarrowConversion {
+public:
+    myItemNarrowConversion(int i, bool b) : m_i(i), m_b(b) {
+        // fprintf(stdout, "[myItemNarrowConversion] ctor with Int and Bool\n");
+    }
+    myItemNarrowConversion(int i, double d) : m_i(i), m_d(d) {
+        // fprintf(stdout, "[myItemNarrowConversion] ctor with Int and Double\n");
+    }
+    myItemNarrowConversion(std::initializer_list<bool> il) {
+        fprintf(stdout, "[myItemNarrowConversion] ctor std::initializer_list\n");
+    }
+    myItemNarrowConversion(const myItemNarrowConversion &other) : m_i(other.i()), m_b(other.b()), m_d(other.d()) {
+        fprintf(stdout, "[myItemNarrowConversion] copy ctor\n");
+    }
+    myItemNarrowConversion(myItemNarrowConversion &&other) : m_i(other.i()), m_b(other.b()), m_d(other.d()) {
+        fprintf(stdout, "[myItemNarrowConversion] move ctor\n");
+    }
+    virtual ~myItemNarrowConversion() { }
+
+public:
+    int i() const { return m_i; }
+    bool b() const { return m_b; }
+    double d() const { return m_d; }
+
+private:
+    int m_i = 100;
+    bool m_b = true;
+    double m_d = 3.14;
+}; // class myItemNarrowConversion
+
+class myItemCallDefaultCtor {
+public:
+    myItemCallDefaultCtor() {
+        fprintf(stdout, "[myItemCallDefaultCtor] ctor with Int and Bool\n");
+    }
+    myItemCallDefaultCtor(std::initializer_list<int> il) {
+        fprintf(stdout, "[myItemCallDefaultCtor] ctor std::initializer_list\n");
+    }
+
+private:
+    int m_i = 100;
+    bool m_b = true;
+    double m_d = 3.14;
+}; // class myItemCallDefaultCtor
+
 void test_ctors();
+
+void test_initialize_ways_compared_with_curly_braces();
+void test_class_member_default_values();
+void test_equal_sign_cannot_be_used_for_constructing_atomic();
+void test_narrowing_conversion();
+void test_curly_braces_hijack_with_std_initializer_list();
+void test_curly_braces_hijack_with_std_initializer_list_lead_to_compile_errors();
+void test_curly_braces_call_default_ctor();
+
+void test_initialize_with_curly_braces();
+
 
 
 void test_all();
