2fa09e6346a12472ee73edf677b60a61bfeaa470
pyrad.asus.msys2 <cailong89@163.com>
2022-10-12 21:36:02 +0800

    Another feature of scoped enum
    Scoped enum (enum class) won't be converted to integral types
    implicitly, while unscoped enum will.

--- src/EffectiveModernCpp/chapter3_item10.cpp
+++ src/EffectiveModernCpp/chapter3_item10.cpp
@@ -1,4 +1,5 @@
 #include "chapter3_item10.hpp"
+#include <cstddef>
 #include <stdio.h>
 
 namespace effective_mordern_cpp {
@@ -6,6 +7,35 @@ namespace effective_mordern_cpp {
 namespace chapter_3 {
 
 namespace item_10 {
+
+/**
+ * A function just to show an unscoped enumrator will be implicitly
+ * converted to integral types(int, uint ...)
+ */
+void pseudo_ret(std::size_t x) {
+    return;
+} // pseudo_ret
+
+/**
+ * To show that unscoped enum will be implicitly converted, while
+ * the scoped enum won't
+ */
+void test_enum_class_no_implicit_convertion() {
+    utilities::ShowStartEndMsg smsg(__FUNCTION__);
+
+    // Unscoped enum
+    Color c = white;
+    if (c < 14.5) {
+        pseudo_ret(c);
+    }
+
+    // Scoped enum
+    EColor ec = EColor::white;
+    if (static_cast<double>(c) < 14.5) {
+        pseudo_ret(static_cast<std::size_t>(c));
+    }
+
+} // test_enum_class_no_implicit_convertion
 
 /**
  * The enumeration values in a enum class won't leak to the scope
@@ -25,6 +55,9 @@ void test_all() {
     utilities::ShowStartEndMsg smsg(__FUNCTION__);
 
     test_enum_class_no_leak();
+
+    test_enum_class_no_implicit_convertion();
+
 } // test_all
 
 } // namespace item_10
--- src/EffectiveModernCpp/chapter3_item10.hpp
+++ src/EffectiveModernCpp/chapter3_item10.hpp
@@ -1,6 +1,7 @@
 #ifndef EFFECTIVE_MODERN_CPP_CHAPTER3_ITEM10
 #define EFFECTIVE_MODERN_CPP_CHAPTER3_ITEM10
 
+#include <cstddef>
 #include <iostream>
 #include "../utilities/utilities.hpp"
 
@@ -9,6 +10,15 @@ namespace effective_mordern_cpp {
 namespace chapter_3 {
 
 namespace item_10 {
+
+enum Color { black, white, red }; // unscoped enum
+enum class EColor { black, white, red }; // scoped enum
+                                         //
+/**
+ * A function just to show an unscoped enumrator will be implicitly
+ * converted to integral types(int, uint ...)
+ */
+void pseudo_ret(std::size_t x);
 
 /**
  * Defines 2 enum classes, which will have a same name "tiger",
@@ -23,6 +33,12 @@ enum class Beast { tiger, lion, cheetah };
  * where the enum class is defined.
  */
 void test_enum_class_no_leak();
+
+/**
+ * To show that unscoped enum will be implicitly converted, while
+ * the scoped enum won't
+ */
+void test_enum_class_no_implicit_convertion();
 
 void test_all();
 
