fc6862f4e46aea395d6f7325bbe35a92a52864b1
pyrad.asus.msys2 <cailong89@163.com>
2022-10-15 19:34:05 +0800

    Finished item 11 for EffectiveModernCpp
    Prefer deleted functions (C++11) to private undefined ones (C++98)

--- src/EffectiveModernCpp/chapter3_item11.cpp
+++ src/EffectiveModernCpp/chapter3_item11.cpp
@@ -1,5 +1,6 @@
 #include "chapter3_item11.hpp"
 #include <stdio.h>
+#include <string>
 
 namespace effective_mordern_cpp {
 
@@ -7,6 +8,9 @@ namespace chapter_3 {
 
 namespace item_11 {
 
+/**
+ * To show how a function is deleted in C++11 & C++98 style
+ */
 void test_cxx98_forbid_func() {
     utilities::ShowStartEndMsg smsg(__FUNCTION__);
 
@@ -16,18 +20,98 @@ void test_cxx98_forbid_func() {
     // That's because we declared the copy-ctor in private and
     // didn't define it.
     //----------
-    // Foo f2(f);
+    // Foo f1(f);
     //----------
 
     fprintf(stdout, "f.id() = %d\n", f.id());
 
+    Foobar f1(10);
+    // If the following statement exist, then compilition fails.
+    // That's because we declared the copy-ctor as deleted
+    // (C++11 styel)
+    //----------
+    // Foobar f3(f1);
+    //----------
+
+    fprintf(stdout, "f1.id() = %d\n", f1.id());
+
 } // test_cxx98_forbid_func
 
+/**
+ * Define a function which only accepts an integer, but doesn't
+ * accepts an argument that can be converted to an integer
+ */
+bool is_lucky(int number) {
+    fprintf(stdout, "The lucky number is %d\n", number);
+    return true;
+} // is_lucky
+
+/**
+ * To show a normal function can be declared as deleted in C++11
+ */
+void test_cxx11_forbid_normal_func() {
+    utilities::ShowStartEndMsg smsg(__FUNCTION__);
+
+    const int num = 12;
+    is_lucky(num);
+
+    // Since "bool is_lucky(double)" is deleted, so the following
+    // can't pass compilition
+    // ---------------
+    // const double num_1 = 1.3;
+    // is_lucky(num_1);
+    // ---------------
+
+    // Since "bool is_lucky(double)" is deleted, so the following
+    // can't pass compilition either, that's because though "bool is_lucky(float)"
+    // is not declared as deleted, compiler will convert a float variable into
+    // a double variable, not an int variable, thus the compile error (use of
+    // delete function) appears
+    // ---------------
+    // const float num_2 = 2.8;
+    // is_lucky(num_2);
+    // ---------------
+
+} // test_cxx11_forbid_normal_func
+
+
+/**
+ * A certain type of template instantiation can be declared as
+ * forbidden in C++11 using "delete" keyword
+ */
+void test_cxx11_forbid_template_instantiation() {
+    utilities::ShowStartEndMsg smsg(__FUNCTION__);
+
+    std::string s("kindle");
+    process_pointer<std::string>(&s);
+
+    // The following call can't be instantiated, because the template instantiation
+    // for type "const char" has been forbidden
+    // ---------------
+    // const char *sp = "good";
+    // process_pointer<const char>(sp);
+    // ---------------
+
+    PtrProcess pp;
+    pp.process_pointer<std::string>(&s);
+
+    // Like above, the following call can't be instantiated, because the template instantiation
+    // for type "const char" has been forbidden
+    // ---------------
+    // const char *sp = "good";
+    // pp.process_pointer<const char>(sp);
+    // ---------------
+
+} // test_cxx11_forbid_template_instantiation
 
 void test_all() {
     utilities::ShowStartEndMsg smsg(__FUNCTION__);
 
     test_cxx98_forbid_func();
+
+    test_cxx11_forbid_normal_func();
+
+    test_cxx11_forbid_template_instantiation();
 
 } // test_all
 
--- src/EffectiveModernCpp/chapter3_item11.hpp
+++ src/EffectiveModernCpp/chapter3_item11.hpp
@@ -3,6 +3,7 @@
 
 #include "../utilities/utilities.hpp"
 #include <stdint.h>
+#include <stdio.h>
 
 namespace effective_mordern_cpp {
 
@@ -37,9 +38,85 @@ private:
 
 }; // class Foo
 
+class Foobar {
+public:
+    Foobar() { }
+    Foobar(const int32_t i) : m_id(i) { }
+    virtual ~Foobar () { }
 
+public:
+    int32_t id() const { return m_id; }
+    void set_id(const int32_t i) { m_id = i; }
+
+    /**
+     * C++11 style
+     * Declare the copy-ctor as deleted
+     */
+    Foobar(const Foobar &) = delete;
+
+private:
+    int32_t m_id;
+}; // class Foobar
+
+/**
+ * To show how a function is deleted in C++11 & C++98 style
+ */
 void test_cxx98_forbid_func();
 
+
+/**
+ * Define a function which only accepts an integer, but doesn't
+ * accepts an argument that can be converted to an integer
+ */
+bool is_lucky(int number); // defined in cpp
+bool is_lucky(char) = delete;
+bool is_lucky(bool) = delete;
+bool is_lucky(double) = delete;
+
+
+/**
+ * "delete" keyword in C++11 can forbid a template instantiation
+ * for a certain type, while no such way to do it in C++98
+ */
+template<typename T> void process_pointer(T *p) {
+    fprintf(stdout, "size of p = %d\n", sizeof(p));
+} // process_pointer
+
+template<> void process_pointer<void>(void *p) = delete;
+template<> void process_pointer<char>(char *p) = delete;
+template<> void process_pointer<const void>(const void *p) = delete;
+template<> void process_pointer<const char>(const char *p) = delete;
+
+
+
+/**
+ * To show how to forbid a template instantiation for a certain inside
+ * a class, only "delete" keyword in C++11 can do it
+ */
+class PtrProcess {
+public:
+    PtrProcess() : m_id(0) {}
+
+    /**
+     * Need to forbid template instantiation for a few types, see below
+     */
+    template<typename T>
+    void process_pointer(T *p) {
+        fprintf(stdout, "size of p = %d\n", sizeof(p));
+    } // process_pointer
+
+private:
+    int32_t m_id;
+
+}; // class PtrProcess
+
+/**
+ * declare them as delete to forbid template instantiation for these types
+ */
+template<> void PtrProcess::process_pointer<void>(void *p) = delete;
+template<> void PtrProcess::process_pointer<char>(char *p) = delete;
+template<> void PtrProcess::process_pointer<const void>(const void *p) = delete;
+template<> void PtrProcess::process_pointer<const char>(const char *p) = delete;
 
 void test_all();
 
