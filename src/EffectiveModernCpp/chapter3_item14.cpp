8b7902bb702d4068d487d8d9d8062e3f34d0a5a9
pyrad.asus.msys2 <cailong89@163.com>
2022-10-18 22:08:01 +0800

    Contine item 14 for EffectiveModernCpp
    Different optimization level for no-exception functions of C++98 style
    and C++11 style

--- src/EffectiveModernCpp/chapter3_item14.cpp
+++ src/EffectiveModernCpp/chapter3_item14.cpp
@@ -10,12 +10,34 @@ namespace chapter_3 {
 namespace item_14 {
 
 
+/**
+ * Compared with C++98 style, the potential benefit for declaring
+ * a function as "noexcept",
+ *
+ * 1. Maybe the call stack will not be kept
+ * 2. Maybe no need to guarantee the reverse order of destroying objects
+ */
+int test_f_noexcept_cxx11(const int x) noexcept { // Most optimized
+    return x * x;
+}
+int test_f_noexcept_cxx98(const int x) throw() { // Less optimized
+    return x * x;
+}
+int test_f_may_except(const int x) { // Less optimized
+    return x * x;
+}
+
 void test_all() {
     utilities::ShowStartEndMsg smsg(__FUNCTION__);
 
     // Use of no exception functions (C++98 style and C++11 style)
     fprintf(stdout, "Square of %d is %d\n", 10, test_func_no_except_cxx98(10));
     fprintf(stdout, "Square of %d is %d\n", 10, test_func_no_except_cxx11(10));
+
+    // Use of no exception and with-exception functions (C++98 style and C++11 style)
+    fprintf(stdout, "Square of %d is %d\n", 10, test_f_noexcept_cxx98(10));
+    fprintf(stdout, "Square of %d is %d\n", 10, test_f_noexcept_cxx11(10));
+    fprintf(stdout, "Square of %d is %d\n", 10, test_f_may_except(10));
 
 } // test_all
 
--- src/EffectiveModernCpp/chapter3_item14.hpp
+++ src/EffectiveModernCpp/chapter3_item14.hpp
@@ -22,9 +22,20 @@ inline int test_func_no_except_cxx98(const int x) throw() {
 /**
  * Shows the no except style in C++11
  */
-inline int test_func_no_except_cxx11(const int x) throw() {
+inline int test_func_no_except_cxx11(const int x) noexcept {
     return x * x;
 }
+
+/**
+ * Compared with C++98 style, the potential benefit for declaring
+ * a function as "noexcept",
+ *
+ * 1. Maybe the call stack will not be kept
+ * 2. Maybe no need to guarantee the reverse order of destroying objects
+ */
+int test_f_noexcept_cxx11(const int x) noexcept; // Most optimized
+int test_f_noexcept_cxx98(const int x) throw(); // Less optimized
+int test_f_may_except(const int x); // Less optimized
 
 
 void test_all();
