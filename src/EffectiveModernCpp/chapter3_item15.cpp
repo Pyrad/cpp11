2e33e0462777319987c746451e023dde7aa6a52b
pyrad.asus.msys2 <cailong89@163.com>
2022-10-21 22:54:15 +0800

    New header & source files for item 15...

---
+++ src/EffectiveModernCpp/chapter3_item15.cpp
@@ -0,0 +1,25 @@
+#ifndef EFFECTIVE_MODERN_CPP_CHAPTER3_ITEM15
+#define EFFECTIVE_MODERN_CPP_CHAPTER3_ITEM15
+
+#include "chapter3_item15.hpp"
+
+namespace effective_mordern_cpp {
+
+namespace chapter_3{
+
+namespace item_15 {
+
+
+void test_all() {
+
+}
+
+
+} // namespace item_15
+
+} // namespace chapter_3
+
+} // namespace effective_mordern_cpp
+
+
+#endif // EFFECTIVE_MODERN_CPP_CHAPTER3_ITEM15
---
+++ src/EffectiveModernCpp/chapter3_item15.hpp
@@ -0,0 +1,21 @@
+#ifndef EFFECTIVE_MODERN_CPP_CHAPTER3_ITEM15
+#define EFFECTIVE_MODERN_CPP_CHAPTER3_ITEM15
+
+#include "../utilities/utilities.hpp"
+
+namespace effective_mordern_cpp {
+
+namespace chapter_3{
+
+namespace item_15 {
+
+void test_all();
+
+} // namespace item_15
+
+} // namespace chapter_3
+
+} // namespace effective_mordern_cpp
+
+
+#endif // EFFECTIVE_MODERN_CPP_CHAPTER3_ITEM15
