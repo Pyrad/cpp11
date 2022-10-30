1790f245c1ddcb201cc4e1cfa5483160a403dd65
pyrad.asus.msys2 <cailong89@163.com>
2022-10-30 13:24:27 +0800

    Item 22, use unique_ptr version of PImpl

--- src/EffectiveModernCpp/chapter4_item22.cpp
+++ src/EffectiveModernCpp/chapter4_item22.cpp
@@ -1,4 +1,5 @@
 #include "chapter4_item22.hpp"
+#include <memory>
 #include <stdio.h>
 #include <vector>
 
@@ -50,6 +51,67 @@ uint32_t FooPImplRawPtr::id() const { return m_pimpl->id(); }
 void FooPImplRawPtr::echo() const { fprintf(stdout, "[FooPImplRawPtr] A foo (ID = %u)\n", m_pimpl->id()); }
 
 
+uint32_t FooPImplUniquePtr::id_cnt = 0;
+
+/**
+ * Implementation of private class FooPImpl in class FooPImplUniquePtr
+ */
+class FooPImplUniquePtr::FooPImpl {
+public:
+    FooPImpl() : m_id(FooPImplUniquePtr::id_cnt++), m_name("unknown") { }
+    FooPImpl(const std::string &n) : m_id(FooPImplUniquePtr::id_cnt++), m_name(n) { }
+    ~FooPImpl() { }
+
+public:
+    uint32_t id() const { return m_id; }
+
+public:
+    uint32_t m_id = 0;
+    std::string m_name = "unknown";
+    std::vector<int> m_ivec;
+}; // end clas FooPImplUniquePtr::FooPImpl
+
+/**
+ * Implementation of the default ctor
+ */
+FooPImplUniquePtr::
+FooPImplUniquePtr() : m_pimpl(std::make_unique<FooPImpl>()) { }
+
+/**
+ * Implementation of the custom ctor
+ */
+FooPImplUniquePtr::
+FooPImplUniquePtr(const std::string &n) : m_pimpl(std::make_unique<FooPImpl>(n)) { }
+
+/**
+ * The dtor implementation is in source file
+ *
+ * Now we use a std::unique_ptr to manange the resource and we don't
+ * have to release the resource on our own.
+ *
+ * But we have to define a destructor here, and implementate it in the
+ * source file, because if we don't provide the default destructor, the
+ * compiler will generate one for us, and in that function the std::unique_ptr
+ * will release the resources.
+ *
+ * That's where the problem is, because in the normal default deleter's
+ * implementation for std::unique_ptr, static_assert() will be used to make
+ * sure the raw pointer it points to is not an incomplete type.
+ * Becuase the special functions are normally delcared as inline, thus the
+ * auxiliar class (FooPImpl) is an incomplete type here, then an error occurs.
+ *
+ * So the solution to this issue it to declare the destructor in the header
+ * file and then implementate it in the source file.
+ * Anothe convenient solution is to just declare it as "=default"
+ */
+FooPImplUniquePtr::~FooPImplUniquePtr() { }
+
+uint32_t FooPImplUniquePtr::id() const { return m_pimpl->id(); }
+
+void FooPImplUniquePtr::echo() const { fprintf(stdout, "[FooPImplUniquePtr] A foo (ID = %u)\n", m_pimpl->id()); }
+
+
+
 void test_pimpl_use_raw_ptr() {
     utilities::ShowStartEndMsg smsg(__FUNCTION__);
 
@@ -62,11 +124,24 @@ void test_pimpl_use_raw_ptr() {
 }
 
 
+void test_pimpl_use_unique_ptr() {
+    utilities::ShowStartEndMsg smsg(__FUNCTION__);
+
+    FooPImplUniquePtr fobj0;
+    FooPImplUniquePtr fobj1("sky");
+
+    fprintf(stdout, "An object of class FooPImplUniquePtr(ID = %u)\n", fobj0.id());
+    fprintf(stdout, "An object of class FooPImplUniquePtr(ID = %u)\n", fobj1.id());
+
+}
+
+
 void test_all() {
     utilities::ShowStartEndMsg smsg(__FUNCTION__);
 
     test_pimpl_use_raw_ptr();
 
+    test_pimpl_use_unique_ptr();
 }
 
 
--- src/EffectiveModernCpp/chapter4_item22.hpp
+++ src/EffectiveModernCpp/chapter4_item22.hpp
@@ -2,6 +2,7 @@
 #define EFFECTIVE_MODERN_CPP_CHAPTER4_ITEM22
 
 #include "../utilities/utilities.hpp"
+#include <memory>
 #include <stdint.h>
 #include <string>
 
@@ -48,7 +49,61 @@ private:
 }; // end class FooPImplRawPtr
 
 
+/**
+ * This is a class using PImpl idiom, by a std::unique_ptr.
+ *
+ * This is the unique_ptr version of FooPImplRawPtr
+ */
+class FooPImplUniquePtr {
+public:
+    static uint32_t id_cnt;
+
+public:
+    /* Default ctor implementation is in source file */
+    FooPImplUniquePtr();
+
+    /* Custom ctor implementation is in source file */
+    FooPImplUniquePtr(const std::string &n);
+
+    /**
+     * The dtor implementation is in source file
+     *
+     * Now we use a std::unique_ptr to manange the resource and we don't
+     * have to release the resource on our own.
+     *
+     * But we have to define a destructor here, and implementate it in the
+     * source file, because if we don't provide the default destructor, the
+     * compiler will generate one for us, and in that function the std::unique_ptr
+     * will release the resources.
+     *
+     * That's where the problem is, because in the normal default deleter's
+     * implementation for std::unique_ptr, static_assert() will be used to make
+     * sure the raw pointer it points to is not an incomplete type.
+     * Becuase the special functions are normally delcared as inline, thus the
+     * auxiliar class (FooPImpl) is an incomplete type here, then an error occurs.
+     *
+     * So the solution to this issue it to declare the destructor in the header
+     * file and then implementate it in the source file.
+     * Anothe convenient solution is to just declare it as "=default"
+     */
+    ~FooPImplUniquePtr();
+
+public:
+    uint32_t id() const;
+    void echo() const;
+
+private:
+    class FooPImpl;
+
+private:
+    std::unique_ptr<FooPImpl> m_pimpl;
+
+}; // end class FooPImplUniquePtr
+
+
 void test_pimpl_use_raw_ptr();
+
+void test_pimpl_use_unique_ptr();
 
 
 void test_all();
