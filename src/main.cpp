f55be80b4ba278de9aa0379eaab974bb9446a543
pyrad.asus.msys2 <cailong89@163.com>
2022-11-29 22:06:27 +0800

    Add 2 more sub directories
    Tried to make shared libs, but didn't succeed, as dll lib can't be found
    when running.
    Currently still make as static linking.
    Should figure out how to make shared libs and link them dynamically.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include "main.hpp"
-#include "normal.hpp"
 #include "template.hpp"
-#include "boosttest.hpp"
-// #include <boost/shared_ptr.hpp>
 #include <memory>
 
 #include "projectconfig.h"
+
+// ------------------------------------------------------
+// The following header files are included conditionally
+// based on the macros defined in CMakeLists.txt
+// ------------------------------------------------------
+#ifdef ENB_NORMAL
+#include "./normal/normal.hpp"
+#endif // ENB_NORMAL
+
+#ifdef ENB_BOOSTTEST
+#include "./boosttest/boosttest.hpp"
+#endif // ENB_BOOSTTEST
+
+#ifdef ENB_CPP_FEATURES
+#include "./cppfeatures/lvalue_rvalue.hpp"
+#endif // ENB_CPP_FEATURES
+
+#ifdef ENB_EFFECTIVE_MODERN_CPP
+#include "./EffectiveModernCpp/effectiveModernCpp.hpp"
+#endif // ENB_EFFECTIVE_MODERN_CPP
+// ------------------------------------------------------
+// Headers included conditionally end
+// ------------------------------------------------------
 
 /**************************************/
 /**  Enable normal feature testing   **/
@@ -23,56 +43,31 @@
 #define ENABLE_TEST_BOOST_FEATURES 0
 #endif // ENABLE_TEST_BOOST_FEATURES
 
-#ifndef SWITCH_ORDER
-#define SWITCH_ORDER 0
-#endif // SWITCH_ORDER
-
-#ifdef ENB_CPP_FEATURES
-#include "./cppfeatures/lvalue_rvalue.hpp"
-#endif // ENB_CPP_FEATURES
-
-#ifdef ENB_EFFECTIVE_MODERN_CPP
-#include "EffectiveModernCpp/effectiveModernCpp.hpp"
-#endif // ENB_EFFECTIVE_MODERN_CPP
-
 int main() {
-
     namespace M = mainspace;
-
     namespace T = template_test;
 
-    namespace B = boost_test;
-
+#ifdef ENB_NORMAL
+    #if ENABLE_TEST_NORMAL_FEATURES
     namespace N = NORMAL;
-
     N::tester_constexpr();
+    N::normal_test_all();
+    M::test_std_regex();
+    #endif // ENABLE_TEST_NORMAL_FEATURES
+#endif // ENB_NORMAL
+
     M::tester_alias_declaration();
     M::tester_auto();
-
-#if SWITCH_ORDER
+    T::tempalte_test_all();
     M::test_all();
 
-    T::tempalte_test_all();
-#else
-    T::tempalte_test_all();
-
-    M::test_all();
-#endif // SWITCH_ORDER
-
-
-#if ENABLE_TEST_NORMAL_FEATURES
-
+#ifdef ENB_BOOSTTEST
+    #if ENABLE_TEST_BOOST_FEATURES
+    namespace B = boost_test;
     B::test_boost_all();
-
-    M::test_std_regex();
-
-    NORMAL::normal_test_all();
-
-#endif // ENABLE_TEST_NORMAL_FEATURES
-
-#if ENABLE_TEST_BOOST_FEATURES
     B::boost_normal_test_all();
-#endif // ENABLE_TEST_BOOST_FEATURES
+    #endif // ENABLE_TEST_BOOST_FEATURES
+#endif // ENB_BOOSTTEST
 
 #ifdef ENB_CPP_FEATURES
     lvalue_rvalue::test_all();
